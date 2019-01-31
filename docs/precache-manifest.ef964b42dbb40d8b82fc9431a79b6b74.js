1d9a4d8226b0e382d277220a70c9284079997e24
algate <361593833@qq.com>
2019-01-31 15:06:50 +0800

    修改build文件路径，便于站点可以找到docs访问路径
    .font-spider 文件夹忽略添加

---
+++ docs/precache-manifest.ef964b42dbb40d8b82fc9431a79b6b74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4697ee22a22fbd7b1ac81a71a2edffaa",
+    "url": "./static/media/handfont.4697ee22.ttf"
+  },
+  {
+    "revision": "73d08ee4f35db379adcc",
+    "url": "./static/js/runtime~main.73d08ee4.js"
+  },
+  {
+    "revision": "7dbea835c9f0dfa6f932",
+    "url": "./static/js/main.7dbea835.chunk.js"
+  },
+  {
+    "revision": "df1ca91a536544be3f44",
+    "url": "./static/js/dynamic.df1ca91a.chunk.js"
+  },
+  {
+    "revision": "4b6a7c6b892d0e02ac86",
+    "url": "./static/js/2.4b6a7c6b.chunk.js"
+  },
+  {
+    "revision": "7dbea835c9f0dfa6f932",
+    "url": "./static/css/main.5a4a4aac.chunk.css"
+  },
+  {
+    "revision": "4e664118140d12049b140cb76b31d846",
+    "url": "./index.html"
+  }
+];
